39e7f647b75fafec5129bf63aabf10e64ff7d678
dillip kumar dora <dillipatpuri@gmail.com>
2017-07-17 13:35:08 +0530

    again

---
+++ shoperFrontend/src/main/java/com/niit/shopperBackend/Controllers/PageController.java
@@ -0,0 +1,18 @@
+package com.niit.shopperBackend.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping(value= {"/","/home","index"})
+	public ModelAndView index(){
+		
+		ModelAndView mv=new ModelAndView("page");
+		mv.addObject("msg","Hello Welcome To Java World");
+		return mv;
+	}
+	
+}
